98b9c94538e67f6a6e9ec68b83a8c599b511d369
David Welch <techy121592@gmail.com>
2017-10-05 19:45:11 -0400

    Renamed a folder

--- py-decryptor/cypherfinder.py
+++ py-decryptor/cypherfinder.py
@@ -6,6 +6,19 @@ class CypherFinder:
         cypher, alphabet = {}, 'abcdefghijklmnopqrstuvwxyz'
         for letter in alphabet.upper() + alphabet:
             cypher[letter] = '?'
+
+        with open(self.corpus_path) as corpus_reader:
+            corpus_data = corpus_reader.read()
+
+        corpus_data
+
+        with open(self.encrypted_file_path) as encrypted_reader:
+            for encrypted_line in encrypted_reader.read():
+                numeric_line = replace_letters_with_numbers(encrypted_line)
+
+
+
+
         print(self.corpus_path)
         print(self.encrypted_file_path)
         return cypher
