2a7890acf535961cb3584e527ee746c9229e574e
David Welch <techy121592@gmail.com>
2017-10-05 19:10:37 -0400

    Added the logic to decrypt a string with a cypher

--- py-decrpytor/decrypter.py
+++ py-decrpytor/decrypter.py
@@ -3,5 +3,11 @@ class Decrypter:
     def __init__(self, cypher):
         self.cypher = cypher
 
+    def swap_letters(self, letter):
+        try:
+            return self.cypher[letter]
+        except:
+            return letter
+
     def decrypt(self, line):
-        return line
+        return ''.join(list(map(self.swap_letters, list(line))))
