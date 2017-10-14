7886117f47a087020aca6365ea52a145a1195227
David Welch <techy121592@gmail.com>
2017-10-14 19:57:05 -0400

    Added copyright notice

--- py-decryptor/cypherfinder.py
+++ py-decryptor/cypherfinder.py
@@ -1,3 +1,19 @@
+#####################################################################################
+# Copyright (C) 2017  David Welch                                                   #
+#                                                                                   #
+# This program is free software; you can redistribute it and/or                     #
+# modify it under the terms of the GNU General Public License                       #
+# as published by the Free Software Foundation; either version 2                    #
+# of the License, or (at your option) any later version.                            #
+# this program is distributed in the hope that it will be useful,                   #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                    #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     #
+# GNU General Public License for more details.                                      #
+# You should have received a copy of the GNU General Public License                 #
+# along with this program; if not, write to the Free Software                       #
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.   #
+#####################################################################################
+
 class CypherFinder:
     def __init__(self, corpus_path):
         self.bag_of_words = self.load_clean_bag_of_words_from_file(corpus_path)
